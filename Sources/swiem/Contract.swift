f95fe88fe0f061b21c9a3d901ce0c142acb7505e
Daksh Kulshrestha <akshd@outlook.com>
2025-07-07 00:42:15 +0530

    chore: update logic

--- Sources/swiem/Contract.swift
+++ Sources/swiem/Contract.swift
@@ -2,21 +2,35 @@ import Foundation
 
 public struct Contract {
     public let address: String
-    public let abi: Data
-    public init(address: String, abi: Data) {
+    public let provider: Provider
+    public init(address: String, provider: Provider) {
         self.address = address
-        self.abi = abi
+        self.provider = provider
     }
 }
 
-public func contract(address: String, abi: Data) -> Contract {
-    Contract(address: address, abi: abi)
+public func contract(address: String, provider: Provider) -> Contract {
+    Contract(address: address, provider: provider)
 }
 
-public func readContract(contract: Contract, function: String, params: [Any]) -> Data? {
-    nil
+public func readContract(contract: Contract, abi: [[String:Any]], function: String, params: [Any], completion: @escaping (Result<Any, Error>) -> Void) {
+    let methodAbi = abi.first { ($0["name"] as? String) == function }!
+    let types = (methodAbi["inputs"] as! [[String:Any]]).map { $0["type"] as! String }
+    let selector = keccak256((function + "(" + types.joined(separator: ",") + ")").data(using: .utf8)!).prefix(4)
+    let encodedArgs = params.enumerated().map { i, v in encodeAbiArg(type: types[i], value: v) }.reduce(Data(), +)
+    let data = selector + encodedArgs
+    let call: [String: Any] = ["to": contract.address, "data": "0x" + data.map { String(format: "%02x", $0) }.joined()]
+    contract.provider.send(method: "eth_call", params: [call, "latest"], completion: completion)
 }
 
-public func writeContract(contract: Contract, function: String, params: [Any], privateKey: Data) -> Data? {
-    nil
+public func writeContract(contract: Contract, abi: [[String:Any]], function: String, params: [Any], wallet: Wallet, completion: @escaping (Result<Any, Error>) -> Void) {
+    let methodAbi = abi.first { ($0["name"] as? String) == function }!
+    let types = (methodAbi["inputs"] as! [[String:Any]]).map { $0["type"] as! String }
+    let selector = keccak256((function + "(" + types.joined(separator: ",") + ")").data(using: .utf8)!).prefix(4)
+    let encodedArgs = params.enumerated().map { i, v in encodeAbiArg(type: types[i], value: v) }.reduce(Data(), +)
+    let data = selector + encodedArgs
+    let to = try! Address(hex: contract.address)
+    let tx = EthereumTransaction(nonce: .zero, gasPrice: .zero, gasLimit: .zero, to: to, value: .zero, data: data, chainId: 1)
+    let raw = try! wallet.sendTransaction(tx: tx)
+    contract.provider.send(method: "eth_sendRawTransaction", params: ["0x" + raw.map { String(format: "%02x", $0) }.joined()], completion: completion)
 } 
